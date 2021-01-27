a6cca09dcb417b39382de3400bf66526328bd3e2
liaoyeguo <liaoyeguo@163.com>
2021-01-28 01:25:31 +0800

    feat: add arrow

--- src/draw-utils/index.ts
+++ src/draw-utils/index.ts
@@ -1,1 +1,14 @@
-export const drawArrow = () => {};
+import p5 from "p5";
+import p5Types from "p5";
+
+export const arrow = (
+  p5: p5Types,
+  position: p5.Vector,
+  direction?: p5.Vector
+) => {
+  p5.push();
+
+  p5.translate(position);
+
+  p5.pop();
+};
