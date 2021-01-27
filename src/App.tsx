2fe731b3b723ca78df6aec47e03cc8c089e45de6
liaoyeguo <2355910+liaoyeguo@users.noreply.github.com>
2021-01-27 23:16:36 +0800

    feat: dispaly name

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Sketch from "react-p5";
 import p5Types from "p5";
+import Participant from "./diagrams/Participant";
 
 export default class App extends Component {
   setup = (p5: p5Types, canvasParentRef: Element) => {
@@ -11,13 +12,19 @@ export default class App extends Component {
     // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
   };
   draw = (p5: p5Types) => {
-    p5.fill(234, 31, 81);
-    p5.noStroke();
-    p5.rect(50, 50, 250, 250);
-    p5.fill(255);
-    p5.textStyle(p5.BOLD);
-    p5.textSize(140);
-    p5.text("p5*", 60, 250);
+    p5.background("#eee");
+    // p5.fill(234, 31, 81);
+    // p5.noStroke();
+    // p5.rect(50, 50, 250, 250);
+    // p5.fill(255);
+    // p5.textStyle(p5.NORMAL);
+    p5.textSize(16);
+    // p5.text("p5*", 60, 250);
+    const p = new Participant({
+      name: "opphkdoakow",
+      position: p5.createVector(p5.width / 2, p5.height / 2)
+    });
+    p.draw(p5);
   };
 
   render() {
--- src/diagrams/Participant.ts
+++ src/diagrams/Participant.ts
@@ -1,15 +1,47 @@
+import p5Types from "p5";
+
 enum ParticipantType {
   PARTICIPANT = "participant",
-  ACTOR = "actor",
+  ACTOR = "actor"
 }
 export default class Participant {
   name: string;
   type: ParticipantType;
+  position: p5Types.Vector;
 
-  constructor(props: { name?: string; type?: ParticipantType }) {
+  constructor(props: {
+    name?: string;
+    type?: ParticipantType;
+    position: p5Types.Vector;
+  }) {
     this.name = props.name || "";
     this.type = props.type ?? ParticipantType.PARTICIPANT;
+    this.position = props.position;
   }
 
-  draw() {}
+  draw(p5: p5Types) {
+    p5.push();
+
+    const fontSize = 16;
+    const originX = this.position.x / 2;
+    const originY = this.position.y / 2;
+    const textWidth = p5.textWidth(this.name);
+    const paddingX = 20;
+    const paddingY = 20;
+    const w = textWidth + paddingX * 2;
+    const h = fontSize + paddingY * 2;
+
+    // 绘制文字
+    p5.textSize(fontSize);
+    p5.fill(0);
+    p5.textAlign(p5.CENTER, p5.CENTER);
+    p5.text(this.name, originX, originY);
+
+    // 绘制bbox
+    p5.rectMode(p5.CENTER);
+    p5.fill(255, 0);
+    p5.rect(originX, originY, w, h);
+
+    p5.pop();
+  }
 }
